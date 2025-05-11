eec6df99230d302b93efbf2a7ba5767b0ed2174e
Juan <jarangoh@unal.edu.co>
2025-05-11 10:41:12 -0500

    Se termina el primer ejercicio implementando las subclases de cuenta con sus atributos y metodos

--- 1.py
+++ 1.py
@@ -44,18 +44,71 @@ class Cuenta():
 #clase hija ahorros
 
 class CuentaAhorros(Cuenta):
+    #atributos
+    _activa:bool
+    #metodos
+    def __init__(self, saldo, tasaAnual):
+        super().__init__(saldo, tasaAnual)
+        self._activa = self._saldo >= 10000
+    def consignar(self, valor):
+        if self._activa:
+            super().consignar(valor)
+            self._activa = self._saldo >= 10000
 
-    pass
+    def retirar(self, valor):
+        if self._activa:
+            super().retirar(valor)
+            self._activa = self._saldo >= 10000
+
+    def extractoMensual(self):
+        if self._numeroRetiros > 4:
+            self._comisionMensual += (self._numeroRetiros - 4) * 1000
+        super().extractoMensual()
+        self._activa = self._saldo >= 10000
+
+    def imprimir(self):
+        total_transacciones = self._numeroConsignaciones + self._numeroRetiros
+        print(f"Cuenta de Ahorros:\nSaldo: ${self._saldo:.2f}\nComisión mensual: ${self._comisionMensual:.2f}\nTransacciones: {total_transacciones}\nActiva: {self._activa}")
+
 
 #clase hija corriente
 
 class CuentaCorriente(Cuenta):
+    _sobregiro:int
 
-    pass
+    def __init__(self, saldo, tasaAnual):
+        super().__init__(saldo, tasaAnual)
+        self._sobregiro = 0
+        
+    def retirar(self, valor: float):
+        if valor > 0:
+            if valor <= self._saldo:
+                self._saldo -= valor
+            else:
+                self._sobregiro += (valor - self._saldo)
+                self._saldo = 0
+            self._numeroRetiros += 1
+
+    def consignar(self, valor: float):
+        if valor > 0:
+            if self._sobregiro > 0:
+                if valor >= self._sobregiro:
+                    valor -= self._sobregiro
+                    self._sobregiro = 0
+                else:
+                    self._sobregiro -= valor
+                    valor = 0
+            self._saldo += valor
+            self._numeroConsignaciones += 1
+
+    def imprimir(self):
+        total_transacciones = self._numeroConsignaciones + self._numeroRetiros
+        print(f"Cuenta Corriente:\nSaldo: ${self._saldo:.2f}\nSobregiro: ${self._sobregiro:.2f}\nComisión mensual: ${self._comisionMensual:.2f}\nTransacciones: {total_transacciones}")
 
 
 
 
+#metodo main
 if __name__ == "__main__":
     cuenta1 = Cuenta(0,10)
     cuenta1.consignar(123)
@@ -63,3 +116,8 @@ if __name__ == "__main__":
     cuenta1.calcularInteresMensual()
     cuenta1.extractoMensual()
     print(f"Saldo: {cuenta1._saldo}\n Número de consignaciones: {cuenta1._numeroConsignaciones}\n Número de retiros: {cuenta1._numeroRetiros}\n Tasa Anual: {cuenta1._tasaAnual}\n Comision Mensual: {cuenta1._comisionMensual} ")
+    cuenta2 = CuentaAhorros(12000,10)
+    cuenta2.consignar(1000)
+    cuenta2.retirar(1000)
+    cuenta2.extractoMensual()
+    cuenta2.imprimir()
